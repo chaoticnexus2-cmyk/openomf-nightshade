0c14ff35fe16dcfc2eb0b29509ece96d3546f8cd
Tuomas Virtanen <katajakasa@gmail.com>
2026-06-02 20:03:56 +0300

    formats: Field renames in AF struct

--- src/resources/af.c
+++ src/resources/af.c
@@ -1,13 +1,12 @@
 #include "formats/af.h"
 #include "resources/af.h"
-#include "resources/sprite.h"
 #include <string.h>
 
 void af_create(af *a, void *src) {
     sd_af_file *sdaf = (sd_af_file *)src;
 
     // Trivial stuff
-    a->id = sdaf->file_id;
+    a->id = sdaf->fighter_id;
     a->endurance = sdaf->endurance;
     a->health = sdaf->health;
     a->forward_speed = sdaf->forward_speed;
@@ -16,7 +15,7 @@ void af_create(af *a, void *src) {
     a->fall_speed = sdaf->fall_speed;
 
     // Sound translation table
-    memcpy(a->sound_translation_table, sdaf->soundtable, 30);
+    memcpy(a->sound_translation_table, sdaf->sound_table, 30);
 
     // Set defaults like master.dat
     // TODO: These may change according to pilot and HAR... Find out how.
